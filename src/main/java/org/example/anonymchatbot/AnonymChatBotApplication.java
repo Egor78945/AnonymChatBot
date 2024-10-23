b5f8d4fa0ca34cbc036de043cd1d9d748ddfa722
egor <Egor78396@gmail.com>
2024-10-23 18:01:50 +0300

    initialized

---
+++ src/main/java/org/example/anonymchatbot/AnonymChatBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.anonymchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymChatBotApplication.class, args);
+    }
+
+}
