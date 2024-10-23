ee71caf870a9c28e6ab04de61851eb340dde5917
egor <Egor78396@gmail.com>
2024-10-23 18:43:25 +0300

    TelegramEnvironment, TelegramConfiguration

---
+++ src/main/java/org/example/anonymchatbot/configuration/telegram/environments/TelegramEnvironment.java
@@ -0,0 +1,23 @@
+package org.example.anonymchatbot.configuration.telegram.environments;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramEnvironment {
+    private final String TELEGRAM_BOT_NAME;
+    private final String TELEGRAM_BOT_TOKEN;
+
+    public TelegramEnvironment(@Value("${telegram.bot.name}") String TELEGRAM_BOT_NAME, @Value("${telegram.bot.token}") String TELEGRAM_BOT_TOKEN) {
+        this.TELEGRAM_BOT_NAME = TELEGRAM_BOT_NAME;
+        this.TELEGRAM_BOT_TOKEN = TELEGRAM_BOT_TOKEN;
+    }
+
+    public String getTELEGRAM_BOT_NAME() {
+        return TELEGRAM_BOT_NAME;
+    }
+
+    public String getTELEGRAM_BOT_TOKEN() {
+        return TELEGRAM_BOT_TOKEN;
+    }
+}
