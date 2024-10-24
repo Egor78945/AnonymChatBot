8e68962265cfa2b9bbbca4f03f4151fafcf612be
egor <Egor78396@gmail.com>
2024-10-24 18:39:46 +0300

    docker-compose-tech.yml(redis), application.yml(redis)

--- src/main/java/org/example/anonymchatbot/AnonymChatBotApplication.java
+++ src/main/java/org/example/anonymchatbot/AnonymChatBotApplication.java
@@ -1,6 +1,5 @@
 package org.example.anonymchatbot;
 
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
